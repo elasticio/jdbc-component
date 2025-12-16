fff3be16492b6b5f1a1db7941b808f479ff5161d
Pavlo Voropaiev <pavel.voropaiev@elastic.io>
2025-12-16 15:47:19 +0200

    fix test

--- src/test/groovy/io/elastic/jdbc/unit/GetRowsPollingTriggerSpec.groovy
+++ src/test/groovy/io/elastic/jdbc/unit/GetRowsPollingTriggerSpec.groovy
@@ -1,17 +1,9 @@
 package io.elastic.jdbc.unit
 
-import io.elastic.api.EventEmitter
-import io.elastic.api.ExecutionParameters
-import io.elastic.api.Message
 import io.elastic.jdbc.triggers.GetRowsPollingTrigger
-import io.elastic.jdbc.utils.Utils
 import spock.lang.Specification
 import jakarta.json.Json
 import jakarta.json.JsonObject
-import java.sql.Connection
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.ResultSetMetaData
 import java.sql.Timestamp
 
 class GetRowsPollingTriggerSpec extends Specification {
@@ -108,40 +100,5 @@ class GetRowsPollingTriggerSpec extends Specification {
         then:
         // matching the expectation from the issue: should detect date and treat as start of day
         result == Timestamp.valueOf("2020-05-15 00:00:00.000")
-    }
-
-    def "should generate correct SQL for MySQL with hire_date"() {
-        given:
-        def emitter = Mock(EventEmitter)
-        def msg = new Message.Builder().build()
-        def config = Json.createObjectBuilder()
-                .add("dbEngine", "mysql")
-                .add("tableName", "employees")
-                .add("pollingField", "hire_date")
-                .add("pollingValue", "2023-01-15")
-                .build()
-        def snapshot = Json.createObjectBuilder().build()
-        def params = new ExecutionParameters(msg, emitter, config, snapshot)
-
-        def connection = Mock(Connection)
-        def preparedStatement = Mock(PreparedStatement)
-        def resultSet = Mock(ResultSet)
-        def metaData = Mock(ResultSetMetaData)
-
-        GroovyMock(Utils, global: true)
-        Utils.getConnection(config) >> connection
-        connection.prepareStatement(_ as String) >> preparedStatement
-        preparedStatement.executeQuery() >> resultSet
-        resultSet.getMetaData() >> metaData
-        metaData.getColumnCount() >> 1
-        metaData.getColumnLabel(1) >> "hire_date"
-        resultSet.next() >>> [true, false]
-        resultSet.getObject("hire_date") >> "2023-01-20"
-        
-        when:
-        trigger.execute(params)
-
-        then:
-        1 * connection.prepareStatement("SELECT * FROM employees WHERE hire_date > ? ORDER BY hire_date ASC LIMIT ?")
     }
 }
